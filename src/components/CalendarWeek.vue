1355424aaf2c49ba2fe37234fe34ec00213223f9
Andy Woerner <andy.woerner@icloud.com>
2020-07-03 23:36:52 +0200

    feat: add components

---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card-group">
+                    <CalendarDay 
+                        v-for="day in state.CalendarWeedData"
+                        :key="day.id"
+                        :day="day"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { store } from "../store";
+import CalendarDay from "./CalendarDay"
+
+export default {
+    name: "CalendarWeek",
+    // innerhalb einer Singel-File-Komponente muss das data-Attribut eine Funktion sein!
+    data() {
+        return {
+            state: store.state,
+
+        }
+    },
+    components: {
+        CalendarDay
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
